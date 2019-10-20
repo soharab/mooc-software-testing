b9c71cc5ebc0fd6b3cf4f8404a050d62a857b0b7
Sohrabuddin Mohammed <mohammed.soharabuddin@moh.gov.om>
2019-10-20 13:05:23 +0400

    Corrected gHappy and CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,19 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
+
+    @ParameterizedTest(name = "input={0},shift={1},result={2}")
+    @CsvSource({"abc,3,def","xyz,3,abc","abc,-3,xyz","abc,0,abc"})
+    public void positiveshift(String message,int shift,String expectedres)
+    {
+        CaesarShiftCipher testObj= new CaesarShiftCipher();
+        String result=testObj.CaesarShiftCipher(message,shift);
+        Assertions.assertEquals(expectedres,result);
+    }
 
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,56 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    void happyTest()
+    {
+        GHappy testObj=new GHappy();
+        boolean result=testObj.gHappy("xxggxx");
+
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    void unhappyTest()
+    {
+        GHappy testObj=new GHappy();
+        boolean result=testObj.gHappy("xxgxx");
+
+        Assertions.assertEquals(false,result);
+    }
+
+    @Test
+    void unhappyTwo()
+    {
+        GHappy testObj=new GHappy();
+        boolean result=testObj.gHappy("xxggyygxx");
+
+        Assertions.assertEquals(false,result);
+
+    }
+
+    @Test
+    void allgorilla()
+    {
+        GHappy testObj=new GHappy();
+        boolean result=testObj.gHappy("gggggg");
+
+        Assertions.assertEquals(true,result);
+    }
+
+    @Test
+    void capitalg()
+    {
+        GHappy testObj=new GHappy();
+        boolean result=testObj.gHappy("xxGGxx");
+
+        Assertions.assertEquals(false,result);
+
+    }
+
+
 }
+
